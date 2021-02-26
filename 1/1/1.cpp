1587a42a437a51137d9f836a37b88684fe558e32
xx123456xx <2359707894@qq.com>
2021-02-26 10:40:44 +0800

    1

---
+++ 1/1/1.cpp
@@ -0,0 +1,13 @@
+#define use _CRT_SECURE_NO_WARNINGS
+	# include<stdio.h>
+int main()
+{
+	int num1=1;
+	int num2=2;
+		int sum=1;
+		scanf("%d%d",&num1,&num2);
+		sum=num1+num2;
+		printf("sum=%d \n",sum);
+	
+	return 0;
+}
