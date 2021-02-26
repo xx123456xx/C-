0fc45dddd7356285db4cb8c110a66bcf8328e7be
xx123456xx <2359707894@qq.com>
2021-02-26 10:46:50 +0800

    代码

---
+++ 下标引用+函数调用+结构成员/下标引用+函数调用+结构成员/下标引用+函数调用+结构成员.h
@@ -0,0 +1,59 @@
+#pragma once
+
+namespace 下标引用函数调用结构成员 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// 下标引用函数调用结构成员 摘要
+	/// </summary>
+	public ref class 下标引用函数调用结构成员 : public System::Windows::Forms::Form
+	{
+	public:
+		下标引用函数调用结构成员(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: 在此处添加构造函数代码
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// 清理所有正在使用的资源。
+		/// </summary>
+		~下标引用函数调用结构成员()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+
+	private:
+		/// <summary>
+		/// 必需的设计器变量。
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// 设计器支持所需的方法 - 不要
+		/// 使用代码编辑器修改此方法的内容。
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = gcnew System::ComponentModel::Container();
+			this->Size = System::Drawing::Size(300,300);
+			this->Text = L"下标引用函数调用结构成员";
+			this->Padding = System::Windows::Forms::Padding(0);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+		}
+#pragma endregion
+	};
+}
